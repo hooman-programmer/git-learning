a83a022d20e2912cccabba65e0328ea96e9fea55
hooman <hooman@mirghasemi.com>
2014-12-19 14:04:39 +0330

    writing  title of index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>This is for learning and practice git with github!</title>
     </head>
     <body>
         <?php
